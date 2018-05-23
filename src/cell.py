eb20a2b15d2f06de9938db811f86343570cccde7
Ryusei Suzuki <nitirin227@gmail.com>
2018-05-24 08:37:08 +0900

    better

--- src/cell.py
+++ src/cell.py
@@ -1,5 +1,6 @@
 class Cell:
     def __init__(self, is_dying):
+        # 0: dead 1: alive
         self.is_dying = is_dying
         self.next_generation = self.is_dying
         self.neighbor_cells = []
@@ -10,11 +11,11 @@ class Cell:
             if not cell.is_dying:
                 alive_count += 1
 
-        if (alive_count <= 1 or 4 <= alive_count) and self.is_dying == 0:
+        if (alive_count <= 1 or 4 <= alive_count) and not self.is_dying:
             self.next_generation = 1
-        elif alive_count == 3 and self.is_dying == 1:
+        elif alive_count == 3 and self.is_dying:
             self.next_generation = 0
-        elif 2 <= self.next_generation <= 3 and self.is_dying == 0:
+        elif 2 <= self.next_generation <= 3 and not self.is_dying:
             self.next_generation = 0
 
     def change_next_generation(self):
